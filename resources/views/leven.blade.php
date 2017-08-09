acab0de9fe71f82c41834cc4ad51a10bff51edaf
Ray van B <thedevraynl@gmail.com>
2017-08-09 16:55:25 +0200

    Initial commit

---
+++ resources/views/leven.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts/app')
+
+@section('content')
+    <h2 class="title">Zoek een leven!</h2>
+    <p>Hier een paar handige links:</p>
+    <ul class="list-unstyled"> 
+        <li>
+            <a href="https://www.randstad.nl">Randstad uitzendbureaus</a>
+        </li>
+        <li><a href="https://www.monsterboard.nl">Monsterboard facatures</a></li>
+    </ul>
+@endsection
